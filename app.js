89ee279934a2f74211884929f68e7dae8c6b7e20
Yifan Wang <namelos@outlook.com>
2016-05-21 21:49:30 +0800

    add server

--- app.js
+++ app.js
@@ -1,43 +1,34 @@
 const http = require('http')
-const Rx = require('@reactivex/rxjs')
+const Cycle = require('@cycle/core')
+const Rx = require('rx')
 
-const makeServerDriver = (port = 3000, callback) => {
-  const requests = new Rx.Subject()
+const makeServerDriver = port => {
+  const req$ = new Rx.Subject()
+  const res$ = new Rx.Subject()
 
-  const serverCallback = (req, res) =>
-          requests.next({ req, res })
+  return sink$ => {
+    http.createServer((req, res) => {
+      req$.onNext(req)
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      sink$.subscribe(sink => {
+        res.end(sink)
+      })
+    }).listen(port, '127.0.0.1', () => console.log(`listening at ${port}`))
 
-  http.createServer(serverCallback)
-    .listen(port, '127.0.0.1', callback)
-
-  return {
-    writeEffect: model =>
-      model.subscribe(({ req, res }) => {
-        console.log('sending hello')
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end('Hello world')
-        return requests
-      }),
-    serverCallback,
-    readEffect: requests
+    return req$
   }
 }
 
-const run = (main, drivers) => {
-  const sources = {
-    server: drivers.server.readEffect
-  }
-  const sinks = main(sources)
-  Object.keys(drivers).forEach(key =>
-                               drivers[key].writeEffect(sinks[key]))
-}
+const makeConsoleDriver = () =>
+        msg$ =>
+        msg$.subscribe(console.log)
 
-const drivers = {
-  server: makeServerDriver(3000, () => console.log('listening on 3000'))
-}
-
-const main = ({ server }) => ({
-  server: server.do(({ req }) => console.log(`request to ${req.url}`))
+const main = ({ console, server }) => ({
+  console: Rx.Observable.of('hello console'),
+  server: Rx.Observable.of('hello server')
 })
 
-run(main, drivers)
+Cycle.run(main, {
+  console: makeConsoleDriver(),
+  server: makeServerDriver(3000)
+})
